1646a129d7d8dfd93bb398e9dcfdcdb2af58f4d3
stagoverflow <stagoverflow@gmail.com>
2022-12-16 10:03:05 -0500

    AoC day 9

--- 09/main.py
+++ 09/main.py
@@ -10,7 +10,7 @@
 # in the same row or column, the tail always moves one step diagonally to keep up:
 
 from enum import Enum
-
+import math
 
 class Direction(Enum):
     UP = 'U'
@@ -41,36 +41,76 @@ def move_head(head_position, direction: Direction):
     else:
         raise(Exception("Invalid direction input"))
 
+
 def move_tail(tail_position, head_position):
+    # TODO: right the head and tail would overlap, really we want the tail to only move one tile
+    # alternatively the tail could move to the spot of the head - 1 step in each direction, that
+    # one step however needs to maintain the direction (+ or -) of the motion
+
+    def _move_lateral(tail, head):
+        motion = head[1] - tail[1]
+        return 0, motion - int(math.copysign(1, motion))  # The tail isn't moving the whole way to the head
+
+    def _move_vertical(tail, head):
+        motion = head[0] - tail[0]
+        return motion - int(math.copysign(1, motion)), 0
+
+    def _add_tuples(tuple_a: tuple, tuple_b: tuple):
+        return tuple(map(lambda x, y: x + y, tuple_a, tuple_b))
+
+    def _move_diagonal(tail, head):
+        return _add_tuples(_move_vertical(tail, head), _move_lateral(tail, head))
+
     if tail_position == head_position:
         return tail_position
     elif tail_position[0] == head_position[0]:
-        tail_position[1] = head_position
+        delta = _move_lateral(tail_position, head_position)
+    elif tail_position[1] == head_position[1]:
+        delta = _move_vertical(tail_position, head_position)
+    else:  # The tail is moving in diagonal
+        delta = _move_diagonal(tail_position, head_position)
+
+    return _add_tuples(tail_position, delta)
 
 
-def parse_grid(input_path, grid_size=100):
-    # A bit of shortcut, we could expand a grid on the fly or something fancy
-    grid = [[0] * grid_size] * grid_size
+def head_tail_contact(tail_position, head_position):
+    """
+    Are head and tail touching
 
-    head_position = (0,0)
-    tail_position = (0,0)
+    :param head_position:
+    :param tail_position:
+    :return: Whether they are touching or not: boolean
+    """
+
+    def _absolute_sub_tuples(tuple_a: tuple, tuple_b: tuple):
+        return tuple(map(lambda x, y: abs(x - y), tuple_a, tuple_b))
+
+    head_to_tail_delta = _absolute_sub_tuples(head_position, tail_position)
+
+    return head_to_tail_delta in [(1, 0), (0, 1), (1, 1), (0, 0)]
+
+
+def track_tail_motion(input_path, grid_size=100):
+    head_position = (0, 0)
+    tail_position = (0, 0)
+    tail_tracker = []
+    head_tracker = []
 
     with open(input_path) as instructions:
         for line in instructions:
-            motions = HeadInput(line)
-
+            motions = HeadInput(line.strip())
             for _ in motions.steps:
                 head_position = move_head(head_position, motions.direction)
-                tail_position = move_tail(tail_position, head_position)
+                head_tracker.append(head_position)
+                if not head_tail_contact(tail_position, head_position):
+                    tail_position = move_tail(tail_position, head_position)
+                    tail_tracker.append(tail_position)
+
+    return tail_tracker, head_tracker
 
 
+tail_tracker, head_tracker = track_tail_motion('input.txt')
 
-
-    pass
-
-
-h = HeadInput("U 3")
-print(h.direction)
-for s in h.steps:
-    print(s)
-
+# print(f"Head moves: {head_tracker}")
+# print(f"Tail moves: {tail_tracker}")
+print(f"Number of unique spots: {len(set(tail_tracker))}")
