b4d58909a69e8273c8e5f78efc211fdbfa065ec7
stagoverflow <stagoverflow@gmail.com>
2022-12-20 17:35:34 -0500

    AoC day 10.. finally!

--- 10/main.py
+++ 10/main.py
@@ -1,18 +1,19 @@
-# 2 cycles to add to x
-# signal strength = cycle number * x
-
-
 def signal_strength(input_path, cycles_of_interest):
     cycle_counter = 1
+    line_counter = 1
     x = 1
-    add_queue = []
     result = 0
 
-    with open(input_path) as commands:
+    add_queue = []
+    history = []
 
+    with open(input_path) as commands:
         line = commands.readline().strip()
 
         while line or add_queue:
+            if cycle_counter in cycles_of_interest:
+                result += x * cycle_counter
+
             if add_queue:
                 x += add_queue.pop()
             else:
@@ -22,14 +23,52 @@ def signal_strength(input_path, cycles_of_interest):
                     add_queue.insert(0, int(instruction[1]))
 
                 line = commands.readline().strip()
+                history.append(instruction)
+                line_counter += 1
 
-            if cycle_counter in cycles_of_interest:
-                result += x * cycle_counter
 
             cycle_counter += 1
-
     return result
 
 
+def render_sprite(input_path):
+    cycle_counter = 1
+    line_counter = 1
+    x = 1
+    add_queue = []
 
-print(signal_strength('sample.txt', [20, 60, 100, 140, 180, 220]))
+    rendered_line = ''
+
+    with open(input_path) as commands:
+
+        line = commands.readline().strip()
+
+        while line or add_queue:
+            horizontal_pixel = (cycle_counter - 1) % 40
+            if horizontal_pixel in (x-1, x, x+1):
+                rendered_line += '#'
+            else:
+                rendered_line += '.'
+
+            if cycle_counter % 40 == 0:
+                print(rendered_line)
+                rendered_line = ''
+
+            if add_queue:
+                x += add_queue.pop()
+            else:
+                instruction = line.split(' ')
+
+                if instruction[0] == 'addx':
+                    add_queue.insert(0, int(instruction[1]))
+
+                line = commands.readline().strip()
+                line_counter += 1
+
+            cycle_counter += 1
+    return
+
+
+print(signal_strength('input.txt', [20, 60, 100, 140, 180, 220]))
+
+render_sprite('input.txt')
