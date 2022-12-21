6829c568851fc567b75db255562ecaa2a7a44b6f
stagoverflow <stagoverflow@gmail.com>
2022-12-21 11:15:41 -0500

    AoC day 11

--- 11/main.py
+++ 11/main.py
@@ -2,12 +2,12 @@ import operator
 import re
 
 class Monkey:
-    def __init__(self, items, operation, divider, true_monkey, false_monkey):
-        self.items = items
-        self.divisible_by = divider
-        self.true_monkey = true_monkey
-        self.false_monkey = false_monkey
-        self.operation = self.parse_operation(operation)
+    # def __init__(self, items, operation, divider, true_monkey, false_monkey):
+        # self.items = items
+        # self.divisible_by = divider
+        # self.true_monkey = true_monkey
+        # self.false_monkey = false_monkey
+        # self.operation = self.parse_operation(operation)
 
     def parse_operation(self, line: str):
         components = str.split(" ")
@@ -36,9 +36,25 @@ def get_numbers_from_string(items: str):
 
 def parse_monkey_activities(input_path):
     monkeys = []
+    current_monkey = None
     with open(input_path) as f:
         for line in f:
             line = line.rstrip()
-            if line.split()[0] == 'Monkey':
+            words = line.split()
+            if words[0] == 'Monkey':
+                current_monkey = Monkey()
+            elif words[0] == 'Starting':
+                current_monkey.items = get_numbers_from_string(line)
+            elif words[0] == 'Operation':
+                current_monkey.operation = current_monkey.parse_operation(line)
+            elif words[0] == 'Test':
+                current_monkey.divider = get_numbers_from_string(line)[0]
+            elif words[1] == 'true':
+                current_monkey.true_monkey = get_numbers_from_string(line)[0]
+            elif words[1] == 'false':
+                current_monkey.false_monkey = get_numbers_from_string(line)
+            else: # else we're done with the current monkey
+                monkeys.append(current_monkey)
+
 
 
