97547f46002921b4ecc974a6abee17f68656cc33
stagoverflow <stagoverflow@gmail.com>
2022-12-21 08:47:38 -0500

    Advent of Code day 11

--- 11/main.py
+++ 11/main.py
@@ -1,3 +1,6 @@
+import operator
+import re
+
 class Monkey:
     def __init__(self, items, operation, divider, true_monkey, false_monkey):
         self.items = items
@@ -8,19 +11,34 @@ class Monkey:
 
     def parse_operation(self, line: str):
         components = str.split(" ")
+        operations = {'*': operator.mul,
+                      '+': operator.add,
+                      '-': operator.sub,
+                      '//': operator.ifloordiv
+        }
 
-        def monkey_op(s, old, new):
-            multiplier = 1
+        def monkey_op(self, old, new):
+            right_hand_side = 1
             if components[-1].isdigit():
-                multiplier = int(components[-1])
-            elif components == 'new':
-                multiplier = new
-            elif components[-1] == 'old'
+                right_hand_side = int(components[-1])
+            elif components[-1] == 'new':
+                right_hand_side = new
+            elif components[-1] == 'old':
+                right_hand_side = old
 
+            return operations[components[-2]](old, right_hand_side)
 
-        return
+        return monkey_op
+
+def get_numbers_from_string(items: str):
+    return re.findall(r'\b\d+\b', items)
 
 
 def parse_monkey_activities(input_path):
+    monkeys = []
     with open(input_path) as f:
         for line in f:
+            line = line.rstrip()
+            if line.split()[0] == 'Monkey':
+
+
